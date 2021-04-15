6b201bc036c431ce36d549f2ccff7e803a770c0c
mohamed-elbermawy <mohamed77bermawy@gmail>
2021-04-16 01:44:34 +0200

    Access Token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+var jwt = require("jsonwebtoken");
 const path = require("path");
 const bcrypt = require("bcrypt");
 const userRouter = require("./routers/user");
@@ -78,11 +79,81 @@ app.get("/login", (req, res) => {
   // }
 });
 
-app.post("/login", (req, res) => {});
+const posts = [
+  {
+    email: "a@gmail.com",
+    title: "post1",
+  },
+  {
+    email: "b@gmail.com",
+    title: "post2",
+  },
+];
+
+let refreshTokens = [];
+
+app.get("/post", authenticateToken, (req, res) => {
+  res.json(posts.filter((post) => post.email === req.user.email));
+});
+
+app.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  console.log(refreshToken);
+  if (refreshToken == null) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken({ email: user.email });
+    res.json({ accessToken: accessToken });
+  });
+});
+
+app.delete("/logout", (req, res) => {
+  refreshTokens = refreshTokens.filter((token) => token !== req.body.token);
+  res.sendStatus(204);
+});
+
+app.post("/login", (req, res) => {
+  //1- Authenticate user
+
+  //2- accessToken
+  const email = req.body.email;
+  const user = { email: email };
+  const accessToken = generateAccessToken(user);
+  const refreshToken = generateRefeshToken(user);
+  refreshTokens.push(refreshToken);
+  console.log(refreshTokens);
+  res.json({ accessToken: accessToken, refreshToken: refreshToken });
+});
 
 app.use("/users", userRouter);
 
 app.use("/posts", postRouter);
+
+// middleware to authenticate Token
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+
+    req.user = user;
+    next();
+  });
+}
+
+// function to generateAccessToken
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "50s" });
+}
+
+// function to generateRefreshToken
+function generateRefeshToken(user) {
+  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+}
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
